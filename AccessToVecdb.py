d40f7d42bf12a96233250d04bed864d687c2e1cd
AAAkater <1318382761@qq.com>
2024-04-12 17:31:21 +0800

    更换图片读取

--- AccessToVecdb.py
+++ AccessToVecdb.py
@@ -1,5 +1,4 @@
 import os
-
 import sys
 import chromadb
 from langchain_community.vectorstores import Chroma
@@ -7,48 +6,54 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter, MarkdownTextSplitter
 
-vecdb_directory: str = os.path.join(os.path.dirname(
-    os.path.abspath(sys.argv[0])), 'vecdb')
-# 连接数据库
-vectordb = Chroma(
-    persist_directory=vecdb_directory,
-    embedding_function=OpenAIEmbeddings()
+vecdb_directory: str = os.path.join(
+    os.path.dirname(os.path.abspath(sys.argv[0])), "vecdb"
 )
-
-print(vectordb._collection.count())
-
-# 语义化搜索
-question = "some girls"
-print(f"{question=}")
-docs = vectordb.max_marginal_relevance_search(question, k=3)
-for doc in docs:
-    print(doc.page_content)
-
-
-# # 访问本地的Chroma
-# persistent_client = chromadb.PersistentClient(path=vecdb_directory)
-
-# # 查看已有集合
-# print(persistent_client.list_collections())
-
-# # 选择langchain
-# collection_name = 'langchain'
-# # 删表
-# # persistent_client.delete_collection(collection_name)
-
-# collection = persistent_client.get_or_create_collection(collection_name)
-
-
-# docs = collection.query(
-#     query_texts=[
-#         'some girls'
-#     ],
-#     n_results=3
+# 连接数据库
+# vectordb = Chroma(
+#     persist_directory=vecdb_directory,
+#     embedding_function=OpenAIEmbeddings()
 # )
-# print(docs)
-# # print(collection.peek(1)['metadatas'])
-# # langchain_chroma = Chroma(
-# #     client=persistent_client,
-# #     collection_name="collection_name",
-# #     embedding_function=OpenAIEmbeddings(),
-# # )
+
+# print(vectordb._collection.count())
+
+# # 语义化搜索
+# question = "some girls"
+# print(f"{question=}")
+# docs = vectordb.max_marginal_relevance_search(question, k=3)
+# for doc in docs:
+#     print(doc.page_content)
+
+
+# 访问本地的Chroma
+persistent_client = chromadb.PersistentClient(path=vecdb_directory)
+
+# 查看已有集合
+print(persistent_client.list_collections())
+
+# 选择langchain
+collection_name = "langchain"
+# 删表
+# persistent_client.delete_collection(collection_name)
+
+collection = persistent_client.get_or_create_collection(collection_name)
+
+
+print(f"行数:{collection.count()}")
+
+
+def print_row(table) -> None:
+    ids = table["ids"]
+    metadatas = table["metadatas"]
+    documents = table["documents"]
+    i = 1
+    for id, metadata, document in zip(ids, metadatas, documents):
+        print(f"序号:{i}")
+        print(f"{id=}")
+        print(f"{metadata['image_path']=}")
+        print(f"{document=}")
+        print()
+        i += 1
+
+
+print_row(collection.get(include=["documents", "metadatas"]))
--- ImageToVecdb.py
+++ ImageToVecdb.py
@@ -13,13 +13,19 @@ from .src.MLLM import tokenizer
 from PIL import Image
 
 # 向量数据库路径
-vecdb_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "vecdb")
+vecdb_directory: str = os.path.join(
+    os.path.dirname(os.path.abspath(sys.argv[0])), "database"
+)
 
 # 存放图像摘要的文件夹的路径
-docs_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "docs")
+docs_directory: str = os.path.join(
+    os.path.dirname(os.path.abspath(sys.argv[0])), "docs"
+)
 
 # 存放图像的文件夹的路径
-imgs_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "imgs")
+imgs_directory: str = os.path.join(
+    os.path.dirname(os.path.abspath(sys.argv[0])), "imgs"
+)
 
 
 def MLLM_answer(image_path: str) -> Document:
@@ -37,45 +43,47 @@ def MLLM_answer(image_path: str) -> Document:
     return doc
 
 
-images = []
-# 读取imgs文件夹下的所有png
-i = 1
-for root, dirs, files in os.walk(imgs_directory):
-    for file in files:
-        if file.endswith(".png"):
-            file_path: str = os.path.join(root, file)
-            try:
-                # loader = ImageCaptionLoader(file_path)
-                # doc = loader.load()
-                # images.extend(doc)
-                doc: Document=MLLM_answer(image_path=file_path)
-                images.extend([doc])
-            except Exception as e:
-                print(f"图片序号:{i} 加载失败")
-                print(e)
-            else:
-                print(f"图片序号:{i} 已加载")
-                i += 1
-                print(f"metadata:{doc.metadata}")
-                print(f"content:{doc.page_content}")
 
-text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
-splits: List[Document] = text_splitter.split_documents(documents=images)
 
-# print(f"{len(splits)=}")
-# print(splits[0])
+if __name__=='__main__':
+    images = []
+    i = 1
+    for root, dirs, files in os.walk(imgs_directory):
+        for file in files:
+            if file.endswith(".png"):
+                file_path: str = os.path.join(root, file)
+                try:
+                    # loader = ImageCaptionLoader(file_path)
+                    # doc = loader.load()
+                    # images.extend(doc)
+                    doc: Document = MLLM_answer(image_path=file_path)
+                    images.extend([doc])
+                except Exception as e:
+                    print(f"图片序号:{i} 加载失败")
+                    print(e)
+                else:
+                    print(f"图片序号:{i} 已加载")
+                    i += 1
+                    print(f"metadata:{doc.metadata}")
+                    print(f"content:{doc.page_content}")
 
-# 存储到向量数据库
-try:
-    vectordb = Chroma.from_documents(
-        documents=splits,
-        embedding=OpenAIEmbeddings(),
-        persist_directory=vecdb_directory,
-    )
-    # 保存到本地
-    vectordb.persist()
-except Exception as e:
-    print("载入数据库失败")
-    print(e)
-else:
-    print("载入数据库成功")
+    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
+    splits: List[Document] = text_splitter.split_documents(documents=images)
+
+    # print(f"{len(splits)=}")
+    # print(splits[0])
+
+    # 存储到向量数据库
+    try:
+        vectordb = Chroma.from_documents(
+            documents=splits,
+            embedding=OpenAIEmbeddings(),
+            persist_directory=vecdb_directory,
+        )
+        # 保存到本地
+        vectordb.persist()
+    except Exception as e:
+        print("载入数据库失败")
+        print(e)
+    else:
+        print("载入数据库成功")
