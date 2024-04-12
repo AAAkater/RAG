13ec1515e439e68d0a656c8cb9532a6db460a859
AAAkater <1318382761@qq.com>
2024-04-12 16:52:07 +0800

    更改MLLM接口

--- ImageToVecdb.py
+++ ImageToVecdb.py
@@ -1,25 +1,40 @@
+import imp
 import os
 import sys
+from typing import List
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter, MarkdownTextSplitter
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.document_loaders import DirectoryLoader
-from langchain.document_loaders.image import UnstructuredImageLoader
+from langchain_community.document_loaders.image import UnstructuredImageLoader
 from langchain_community.document_loaders import ImageCaptionLoader
+from langchain_community.docstore.document import Document
+from .src.MLLM import model
+from .src.MLLM import tokenizer
+from PIL import Image
 
 # 向量数据库路径
-vecdb_directory: str = os.path.join(
-    os.path.dirname(os.path.abspath(sys.argv[0])), "vecdb"
-)
+vecdb_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "vecdb")
 
 # 存放图像摘要的文件夹的路径
-docs_directory = os.path.join(os.path.dirname(
-    os.path.abspath(sys.argv[0])), "docs")
+docs_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "docs")
 
 # 存放图像的文件夹的路径
-imgs_directory = os.path.join(os.path.dirname(
-    os.path.abspath(sys.argv[0])), "imgs")
+imgs_directory: str = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "imgs")
+
+
+def MLLM_answer(image_path: str) -> Document:
+    """
+    Parameters:
+    image_path (str): 图片路径
+
+    Returns:
+        Document
+    """
+    image = model.encode_image(Image.open(fp=image_path))
+    question = "Generate a summary of this image"
+    answer = model.answer_question(image, question, tokenizer)
+    doc = Document(page_content=answer, metadata={"image_path": file_path})
+    return doc
 
 
 images = []
@@ -28,22 +43,24 @@ i = 1
 for root, dirs, files in os.walk(imgs_directory):
     for file in files:
         if file.endswith(".png"):
-            file_path = os.path.join(root, file)
+            file_path: str = os.path.join(root, file)
             try:
-                loader = ImageCaptionLoader(file_path)
-                tmp = loader.load()
-                images.extend(tmp)
+                # loader = ImageCaptionLoader(file_path)
+                # doc = loader.load()
+                # images.extend(doc)
+                doc: Document=MLLM_answer(image_path=file_path)
+                images.extend([doc])
             except Exception as e:
-                print(f"序号:{i} 加载失败")
+                print(f"图片序号:{i} 加载失败")
                 print(e)
             else:
-                print(f"序号:{i} 已加载")
+                print(f"图片序号:{i} 已加载")
                 i += 1
-                print(f"metadata:{tmp[0].metadata}")
-                print(f"content:{tmp[0].page_content}")
+                print(f"metadata:{doc.metadata}")
+                print(f"content:{doc.page_content}")
 
 text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
-splits = text_splitter.split_documents(images)
+splits: List[Document] = text_splitter.split_documents(documents=images)
 
 # print(f"{len(splits)=}")
 # print(splits[0])
