2e129a0050b82ec60f70bf8cd9e47651e4823c24
AAAkater <1318382761@qq.com>
2024-04-13 17:36:45 +0800

    更换接口

--- ImageToVecdb.py
+++ ImageToVecdb.py
@@ -1,4 +1,3 @@
-import imp
 import os
 import sys
 from typing import List
@@ -8,9 +7,9 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders.image import UnstructuredImageLoader
 from langchain_community.document_loaders import ImageCaptionLoader
 from langchain_community.docstore.document import Document
-from src.MLLM import model
-from src.MLLM import tokenizer
 from PIL import Image
+from src.MLLM import MLLM_model
+from src.Vectordb import vecdb
 
 # 向量数据库路径
 vecdb_directory: str = os.path.join(
@@ -28,24 +27,12 @@ imgs_directory: str = os.path.join(
 )
 
 
-def MLLM_answer(image_path: str) -> Document:
-    """
-    Parameters:
-    image_path (str): 图片路径
+if __name__ == "__main__":
 
-    Returns:
-        Document
-    """
-    image = model.encode_image(Image.open(fp=image_path))
-    question = "Generate a summary of this image"
-    answer = model.answer_question(image, question, tokenizer)
-    doc = Document(page_content=answer, metadata={"image_path": file_path})
-    return doc
+    MLLM = MLLM_model()
+    db = vecdb(collection_name="langchain")
+    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
 
-
-
-
-if __name__=='__main__':
     images = []
     i = 1
     for root, dirs, files in os.walk(imgs_directory):
@@ -53,11 +40,8 @@ if __name__=='__main__':
             if file.endswith(".png"):
                 file_path: str = os.path.join(root, file)
                 try:
-                    # loader = ImageCaptionLoader(file_path)
-                    # doc = loader.load()
-                    # images.extend(doc)
-                    doc: Document = MLLM_answer(image_path=file_path)
-                    images.extend([doc])
+                    doc: Document = MLLM.answer(file_path)
+                    db.add(doc)
                 except Exception as e:
                     print(f"图片序号:{i} 加载失败")
                     print(e)
@@ -67,23 +51,20 @@ if __name__=='__main__':
                     print(f"metadata:{doc.metadata}")
                     print(f"content:{doc.page_content}")
 
-    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
-    splits: List[Document] = text_splitter.split_documents(documents=images)
-
     # print(f"{len(splits)=}")
     # print(splits[0])
 
     # 存储到向量数据库
-    try:
-        vectordb = Chroma.from_documents(
-            documents=splits,
-            embedding=OpenAIEmbeddings(),
-            persist_directory=vecdb_directory,
-        )
-        # 保存到本地
-        vectordb.persist()
-    except Exception as e:
-        print("载入数据库失败")
-        print(e)
-    else:
-        print("载入数据库成功")
+    # try:
+    #     vectordb = Chroma.from_documents(
+    #         documents=splits,
+    #         embedding=OpenAIEmbeddings(),
+    #         persist_directory=vecdb_directory,
+    #     )
+    #     # 保存到本地
+    #     vectordb.persist()
+    # except Exception as e:
+    #     print("载入数据库失败")
+    #     print(e)
+    # else:
+    #     print("载入数据库成功")
