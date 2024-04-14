19f87b31abeb61dc7dce16830dfb3f409e24c88d
AAAkater <1318382761@qq.com>
2024-04-14 20:06:28 +0800

    修改prompts

--- src/LLM.py
+++ src/LLM.py
@@ -4,8 +4,6 @@ from langchain.chains import RetrievalQA
 
 
 class LLM_model:
-    question: str = "Is there any content about girls?"
-
     def __init__(self, db_retriever) -> None:
         self.llm = ChatOpenAI(temperature=0)
         self.qa_chain = RetrievalQA.from_chain_type(
@@ -15,5 +13,5 @@ class LLM_model:
             return_source_documents=True,
         )
 
-    def invoke(self, question):
-        return self.qa_chain.invoke(question)
+    def invoke(self, input):
+        return self.qa_chain.invoke(f"give me some information about the {input}")
