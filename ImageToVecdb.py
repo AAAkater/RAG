5945045b79443c1b42cdadf7134081da03015b37
AAAkater <1318382761@qq.com>
2024-04-14 16:48:18 +0800

    添加to_png的修改

--- ImageToVecdb.py
+++ ImageToVecdb.py
@@ -8,6 +8,7 @@ from langchain_community.document_loaders.image import UnstructuredImageLoader
 from langchain_community.docstore.document import Document
 from src.MLLM import MLLM_model
 from src.Vectordb import vecdb
+from src.ToPng import to_png
 
 # 存放图片文件夹的位置
 images_path = "./imgs/"
@@ -16,21 +17,16 @@ if __name__ == "__main__":
 
     MLLM = MLLM_model()
     collection = vecdb(collection_name="langchain")
-    text_splitter = CharacterTextSplitter(chunk_size=10, chunk_overlap=5)
-
-    i = 1
-    for root, dirs, files in os.walk(images_path):
-        for file in files:
-            if file.endswith(".png"):
-                file_path: str = os.path.join(root, file)
-                try:
-                    doc: Document = MLLM.answer(file_path)
-                    collection.add(doc)
-                except Exception as e:
-                    print(f"图片序号:{i} 加载失败")
-                    print(e)
-                else:
-                    print(f"图片序号:{i} 已加载")
-                    i += 1
-                    print(f"metadata:{doc.metadata}")
-                    print(f"content:{doc.page_content}")
+    to_png(images_path)
+    for i, file_name in enumerate(os.listdir(images_path)):
+        file_path: str = os.path.join(images_path, file_name)
+        try:
+            doc: Document = MLLM.answer(file_path)
+            collection.add(doc)
+        except Exception as e:
+            print(f"图片序号:{i} 加载失败")
+            print(e)
+        else:
+            print(f"图片序号:{i} 已加载")
+            print(f"metadata:{doc.metadata}")
+            print(f"content:{doc.page_content}")
