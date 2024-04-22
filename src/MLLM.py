4f3e9b8b02af352203e8b197244d12439da0390b
AAAkater <1318382761@qq.com>
2024-04-23 00:52:45 +0800

    更改模型名

--- src/MLLM.py
+++ src/MLLM.py
@@ -6,16 +6,15 @@ from PIL import Image
 
 
 # 选择MLLM模型
-class MLLM_model:
+class moondream:
     _model_id = "vikhyatk/moondream2"  # 模型id
     _revision = "2024-04-02"  # 模型版本
     _model: Any = None  # 模型
     _tokenizer: Any = None  # 分词器
-    question: str = "Generate a summary of this image"
+    prompt: str = "Generate a summary of this image"
 
     def __init__(self):
         try:
-            # 模型
             self._model = AutoModelForCausalLM.from_pretrained(
                 pretrained_model_name_or_path=self._model_id,
                 trust_remote_code=True,
@@ -26,10 +25,7 @@ class MLLM_model:
                 pretrained_model_name_or_path=self._model_id, revision=self._revision
             )
         except Exception as e:
-            print("连接MLLM失败 网络不稳定")
-            print(e)
-        else:
-            print("连接MLLM成功")
+            raise RuntimeError(f"MLLM初始化失败:{e}")
 
     def answer(self, image_path: str) -> Document:
         """输入图片让MLLM返回相关消息
@@ -42,11 +38,31 @@ class MLLM_model:
         """
 
         image = self._model.encode_image(Image.open(fp=image_path))
-        answer = self._model.answer_question(image, self.question, self._tokenizer)
+        answer = self._model.answer_question(image, self.prompt, self._tokenizer)
         doc = Document(page_content=answer, metadata={"image_path": image_path})
         return doc
 
+    def answer_print(self, image_path: str):
+        """输入图片让MLLM返回相关消息并打印
+
+        Args:
+            image_path (str): 图片路径
+        """
+        image = self._model.encode_image(Image.open(fp=image_path))
+        answer = self._model.answer_question(image, self.prompt, self._tokenizer)
+        print(f"{image_path=}")
+        print(f"query:{self.prompt}", end="\n\n")
+        print(f"answer:{answer}")
+
 
 if __name__ == "__main__":
-    mllm = MLLM_model()
-    print(mllm.answer("F:\\Desktop\\vsc\\python\\RAG\\imgs\\cry2.png"))
+    """
+    测试
+    """
+    try:
+        mllm = moondream()
+    except RuntimeError as e:
+        print(e)
+    else:
+        print(f"MLLM初始化成功")
+        mllm.answer_print(image_path="./imgs/girls/angry.png")
