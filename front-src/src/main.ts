10a179d5045e636d2bfec27716b20e8eea237f78
AAAkater <1318382761@qq.com>
2024-09-20 15:23:57 +0800

    ✨ feat: add pinia

--- front-src/src/main.ts
+++ front-src/src/main.ts
@@ -1,12 +1,14 @@
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/reset.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import './main.css'
-import router from './router'
+import Antd from "ant-design-vue"
+import "ant-design-vue/dist/reset.css"
+import { createApp } from "vue"
+import App from "./App.vue"
+import "./main.css"
+import router from "./router"
+import pinia from "./stores"
 
 const app = createApp(App)
-
+app.use(Antd)
+app.use(pinia)
 app.use(router)
 
-app.use(Antd).mount('#app')
+app.mount("#app")
