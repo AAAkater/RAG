b963de77d96c84f9cf9d2060e7632ee7f0954465
AAAkater <1318382761@qq.com>
2024-09-21 12:07:05 +0800

    🐞 fix: eslint config

--- front-src/vite.config.ts
+++ front-src/vite.config.ts
@@ -1,22 +1,16 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
+import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [AntDesignVueResolver({ importStyle: false })]
-    })
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   // server: {
   //   proxy: {
   //     '/api': {
