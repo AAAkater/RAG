c531578ee2c6bab0394e8b7eb24bfec07f4da883
AAAkater <1318382761@qq.com>
2024-09-21 20:46:12 +0800

    ✨ feat: add dashboard layout

---
+++ front-src/src/views/dashboard/components/MenuLogo.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = defineProps({
+  collapsed: {
+    default: false,
+    required: true,
+    type: Boolean,
+  },
+})
+</script>
+
+<template>
+  <div class="my-2 flex justify-evenly">
+    <img
+      src="@/assets/icons/logo.svg"
+      alt=""
+      class="h-10 w-auto"
+    />
+    <div
+      v-if="!props.collapsed"
+      class="flex items-center font-mono text-3xl text-white"
+    >
+      RAG
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- front-src/src/views/dashboard/index.vue
+++ front-src/src/views/dashboard/index.vue
@@ -1,7 +1,69 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { ref } from "vue"
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from "@ant-design/icons-vue"
+import MenuLogo from "./components/MenuLogo.vue"
+import { Layout, Menu } from "ant-design-vue"
+const selectedKeys = ref<string[]>(["1"])
+const collapsed = ref<boolean>(false)
+</script>
 
 <template>
-  <div>仪表盘</div>
+  <Layout>
+    <Layout.Sider
+      v-model:collapsed="collapsed"
+      :trigger="null"
+      collapsible
+      class="h-screen"
+    >
+      <MenuLogo :collapsed="collapsed" />
+      <Menu
+        v-model:selectedKeys="selectedKeys"
+        theme="dark"
+        mode="inline"
+      >
+        <Menu.Item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </Menu.Item>
+      </Menu>
+    </Layout.Sider>
+    <Layout>
+      <Layout.Header style="background: #fff">
+        <menu-unfold-outlined
+          v-if="collapsed"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <menu-fold-outlined
+          v-else
+          @click="() => (collapsed = !collapsed)"
+        />
+      </Layout.Header>
+      <Layout.Content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '280px',
+        }"
+      >
+        Content
+      </Layout.Content>
+    </Layout>
+  </Layout>
 </template>
 
 <style scoped></style>
