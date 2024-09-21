fe8ffb10383663572ba96408eb0d73f25700140b
AAAkater <1318382761@qq.com>
2024-09-21 20:51:39 +0800

    🌈 style: format

--- front-src/src/views/deprecated/HomeView.vue
+++ front-src/src/views/deprecated/HomeView.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import MetaDisplay from '@/components/MetaDisplay.vue'
-import { apiBase } from '@/constants'
-import type { ContextItem } from '@/types'
-import openNotification from '@/util/openNotification'
+import MetaDisplay from "@/components/MetaDisplay.vue"
+import { apiBase } from "@/constants"
+import type { ContextItem } from "@/types"
+import openNotification from "@/util/openNotification"
 
-import axios from 'axios'
-import { nextTick, ref } from 'vue'
+import axios from "axios"
+import { nextTick, ref } from "vue"
 
 const context = ref<ContextItem[]>([])
 
-const input = ref('')
+const input = ref("")
 const isHover = ref(false)
 const isLoading = ref(false)
 const scrollRef = ref<HTMLDivElement | null>(null)
 const uploadFile = ref<File | null>()
 
 function readFile() {
-  const inputFile = document.querySelector<HTMLInputElement>('#myFile')
+  const inputFile = document.querySelector<HTMLInputElement>("#myFile")
   let file = inputFile?.files?.[0]
   uploadFile.value = file
   console.log(uploadFile.value)
@@ -25,110 +25,120 @@ function readFile() {
 async function handleUpload(type: string) {
   if (uploadFile.value) {
     if (
-      (type === 'Document' && uploadFile.value.type !== 'application/pdf') ||
-      (type === 'Video' && uploadFile.value.type !== 'video/mp4') ||
-      (type === 'Image' &&
-        uploadFile.value.type !== 'image/png' &&
-        uploadFile.value.type !== 'image/jpeg') ||
-      (type === 'Audio' && uploadFile.value.type !== 'audio/wav')
+      (type === "Document" && uploadFile.value.type !== "application/pdf") ||
+      (type === "Video" && uploadFile.value.type !== "video/mp4") ||
+      (type === "Image" &&
+        uploadFile.value.type !== "image/png" &&
+        uploadFile.value.type !== "image/jpeg") ||
+      (type === "Audio" && uploadFile.value.type !== "audio/wav")
     ) {
-      openNotification('topRight', '请检查上传文件类型！', false)
+      openNotification("topRight", "请检查上传文件类型！", false)
       return
     }
 
     let formData = new FormData()
-    formData.append('file', uploadFile.value)
+    formData.append("file", uploadFile.value)
     await axios
-      .post(apiBase + '/upload' + type, formData, {
+      .post(apiBase + "/upload" + type, formData, {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          "Content-Type": "multipart/form-data",
         },
-        timeout: 5000
+        timeout: 5000,
       })
       .then((response) => {
         console.log(response.data)
-        openNotification('topRight', '上传成功！', true)
+        openNotification("topRight", "上传成功！", true)
       })
       .catch((e) => {
         console.log(e)
-        openNotification('topRight', '上传失败，请求错误！', false)
+        openNotification("topRight", "上传失败，请求错误！", false)
         return
       })
   }
 }
 
 async function handleSend() {
-  if (input.value.trim() === '') {
+  if (input.value.trim() === "") {
     return
   }
 
   isLoading.value = true
 
   context.value.push({
-    from: 'user',
-    data: input.value
+    from: "user",
+    data: input.value,
   })
 
   nextTick(() =>
-    scrollRef.value?.scrollTo({ top: scrollRef.value?.scrollHeight, behavior: 'smooth' })
+    scrollRef.value?.scrollTo({
+      top: scrollRef.value?.scrollHeight,
+      behavior: "smooth",
+    }),
   )
   const resp = await axios
-    .get(apiBase + '/query', { timeout: 5000, params: { desc: input.value } })
+    .get(apiBase + "/query", { timeout: 5000, params: { desc: input.value } })
     .then((response) => response.data)
     .catch((error: any) => {
       console.log(error.code)
       isLoading.value = false
 
-      if (error.code == 'ECONNABORTED') {
+      if (error.code == "ECONNABORTED") {
         context.value.push({
-          from: 'assistant',
-          data: '请求超时，请检测当前网络环境。',
+          from: "assistant",
+          data: "请求超时，请检测当前网络环境。",
           metadata: [],
           success: false,
-          inquiryContent: input.value
+          inquiryContent: input.value,
         })
       } else {
         context.value.push({
-          from: 'assistant',
-          data: '请求错误，请联系工作人员。',
+          from: "assistant",
+          data: "请求错误，请联系工作人员。",
           metadata: [],
           success: false,
-          inquiryContent: input.value
+          inquiryContent: input.value,
         })
       }
-      input.value = ''
+      input.value = ""
 
       return
     })
   context.value.push({
-    from: 'assistant',
+    from: "assistant",
     data: resp.data.answer,
     metadata: resp.data.metadata,
-    success: true
+    success: true,
   })
 
   isLoading.value = false
-  input.value = ''
+  input.value = ""
   console.log(context.value)
 
   nextTick(() =>
-    scrollRef.value?.scrollTo({ top: scrollRef.value?.scrollHeight, behavior: 'smooth' })
+    scrollRef.value?.scrollTo({
+      top: scrollRef.value?.scrollHeight,
+      behavior: "smooth",
+    }),
   )
 }
 
 async function handleClear() {
-  await axios.post(apiBase + '/clear')
+  await axios.post(apiBase + "/clear")
   context.value = []
 }
 </script>
 
 <template>
-  <main class="bg-white border-x max-w-screen-md mx-auto h-[100vh] flex flex-col items-center">
+  <main
+    class="mx-auto flex h-[100vh] max-w-screen-md flex-col items-center border-x bg-white"
+  >
     <div
       v-if="context.length === 0"
-      class="flex flex-col items-center justify-center gap-8 flex-grow"
+      class="flex flex-grow flex-col items-center justify-center gap-8"
     >
-      <div class="rounded-full border w-20 h-20 flex items-center justify-center">
+      <div
+        class="flex h-20 w-20 items-center justify-center rounded-full border"
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="42"
@@ -141,7 +151,9 @@ async function handleClear() {
           stroke-linejoin="round"
         >
           <path d="M12 6V2H8" />
-          <path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z" />
+          <path
+            d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z"
+          />
           <path d="M2 12h2" />
           <path d="M9 11v2" />
           <path d="M15 11v2" />
@@ -151,22 +163,38 @@ async function handleClear() {
       <span class="text-2xl">基于langchain的多模态知识库检索系统</span>
       <div class="flex flex-col items-center gap-3">
         <span class="text-lg">想要检索些什么</span>
-        <ul class="list-disc pl-5 space-y-2 text-gray-500">
-          <li class="text-xs">资源检索：在查找栏输入您要查找的关键字，然后单击“发送”按钮。</li>
-          <li class="text-xs">下载资源：在搜索结果中，您可以通过点击“下载”按钮将所需资源.</li>
-          <li class="text-xs">错误处理：如果搜索请求未能成功执行，系统会显示错误信息。</li>
-          <li class="text-xs">资源上传：您可以上传所需文件类型以扩大我们的数据库。</li>
-          <li class="text-xs">消息清理：您可以通过点击右侧垃圾箱图标清空所有消息记录。</li>
+        <ul class="list-disc space-y-2 pl-5 text-gray-500">
+          <li class="text-xs">
+            资源检索：在查找栏输入您要查找的关键字，然后单击“发送”按钮。
+          </li>
+          <li class="text-xs">
+            下载资源：在搜索结果中，您可以通过点击“下载”按钮将所需资源.
+          </li>
+          <li class="text-xs">
+            错误处理：如果搜索请求未能成功执行，系统会显示错误信息。
+          </li>
+          <li class="text-xs">
+            资源上传：您可以上传所需文件类型以扩大我们的数据库。
+          </li>
+          <li class="text-xs">
+            消息清理：您可以通过点击右侧垃圾箱图标清空所有消息记录。
+          </li>
         </ul>
       </div>
     </div>
 
-    <div v-else class="flex flex-col flex-grow relative w-full">
-      <div class="absolute inset-0 overflow-auto" ref="scrollRef">
+    <div
+      v-else
+      class="relative flex w-full flex-grow flex-col"
+    >
+      <div
+        class="absolute inset-0 overflow-auto"
+        ref="scrollRef"
+      >
         <div
           v-for="(item, index) in context"
           :key="index + item.from + item.data"
-          class="p-4 flex gap-2 border-b"
+          class="flex gap-2 border-b p-4"
           :class="{ 'flex-row-reverse': item.from !== 'assistant' }"
         >
           <svg
@@ -180,10 +208,14 @@ async function handleClear() {
             stroke-width="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="opacity-75 flex-shrink-0"
+            class="flex-shrink-0 opacity-75"
           >
             <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-            <circle cx="12" cy="7" r="4" />
+            <circle
+              cx="12"
+              cy="7"
+              r="4"
+            />
           </svg>
           <svg
             v-else
@@ -196,22 +228,32 @@ async function handleClear() {
             stroke-width="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="opacity-75 flex-shrink-0"
+            class="flex-shrink-0 opacity-75"
           >
             <path d="M12 6V2H8" />
-            <path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z" />
+            <path
+              d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z"
+            />
             <path d="M2 12h2" />
             <path d="M9 11v2" />
             <path d="M15 11v2" />
             <path d="M20 12h2" />
           </svg>
           <div class="flex flex-grow flex-col">
-            <div v-if="item.from === 'assistant'" class="self-start">
-              <div class="text-sm h-5 leading-5 opacity-75">Multimodality RAG</div>
-              <div class="mt-2 bg-blue-600 p-2 rounded-lg text-white">
+            <div
+              v-if="item.from === 'assistant'"
+              class="self-start"
+            >
+              <div class="h-5 text-sm leading-5 opacity-75">
+                Multimodality RAG
+              </div>
+              <div class="mt-2 rounded-lg bg-blue-600 p-2 text-white">
                 {{ item.data }}
               </div>
-              <div v-if="!item.success" class="text-sm h-5 leading-5 hover:opacity-30 w-auto">
+              <div
+                v-if="!item.success"
+                class="h-5 w-auto text-sm leading-5 hover:opacity-30"
+              >
                 <svg
                   @click="
                     () => {
@@ -235,25 +277,42 @@ async function handleClear() {
               </div>
               <div
                 v-if="item && item.metadata && item.metadata.length !== 0"
-                class="mt-2 border rounded-lg p-2 flex flex-row gap-2 flex-wrap"
+                class="mt-2 flex flex-row flex-wrap gap-2 rounded-lg border p-2"
               >
                 <div
-                  v-for="_item in item.metadata.sort((a, b) => a.type.localeCompare(b.type))"
+                  v-for="_item in item.metadata.sort((a, b) =>
+                    a.type.localeCompare(b.type),
+                  )"
                   :key="_item.id"
                   :class="{ 'w-full': _item.type !== 'image' }"
                 >
-                  <MetaDisplay :type="_item.type" :filename="_item.filename" :id="_item.id" />
+                  <MetaDisplay
+                    :type="_item.type"
+                    :filename="_item.filename"
+                    :id="_item.id"
+                  />
                 </div>
               </div>
             </div>
-            <div v-else class="self-end">
-              <div class="text-sm h-5 leading-5 opacity-75" style="direction: rtl">你</div>
-              <div class="mt-2 bg-gray-200 p-2 rounded-lg">{{ item.data }}</div>
+            <div
+              v-else
+              class="self-end"
+            >
+              <div
+                class="h-5 text-sm leading-5 opacity-75"
+                style="direction: rtl"
+              >
+                你
+              </div>
+              <div class="mt-2 rounded-lg bg-gray-200 p-2">{{ item.data }}</div>
             </div>
           </div>
         </div>
 
-        <div v-if="isLoading" class="p-4 flex gap-2 border-b">
+        <div
+          v-if="isLoading"
+          class="flex gap-2 border-b p-4"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="20"
@@ -264,10 +323,12 @@ async function handleClear() {
             stroke-width="1.5"
             stroke-linecap="round"
             stroke-linejoin="round"
-            class="opacity-75 flex-shrink-0"
+            class="flex-shrink-0 opacity-75"
           >
             <path d="M12 6V2H8" />
-            <path d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z" />
+            <path
+              d="m8 18-4 4V8a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2Z"
+            />
             <path d="M2 12h2" />
             <path d="M9 11v2" />
             <path d="M15 11v2" />
@@ -275,7 +336,7 @@ async function handleClear() {
           </svg>
           <div class="flex flex-grow flex-col">
             <div>
-              <div class="text-sm h-5 leading-5 opacity-75">Image RAG</div>
+              <div class="h-5 text-sm leading-5 opacity-75">Image RAG</div>
               <div class="mt-2 flex gap-2 opacity-50">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -299,11 +360,11 @@ async function handleClear() {
       </div>
     </div>
 
-    <div class="w-full p-4 flex gap-2 items-center">
+    <div class="flex w-full items-center gap-2 p-4">
       <div class="w-auto">
         <div
           v-if="isHover"
-          class="absolute w-auto h-auto bg-gray-200 rounded-lg bottom-[70px] flex flex-col justify-start items-start p-2 drop-shadow-xl"
+          class="absolute bottom-[70px] flex h-auto w-auto flex-col items-start justify-start rounded-lg bg-gray-200 p-2 drop-shadow-xl"
         >
           <svg
             @click="(isHover = false), (uploadFile = null)"
@@ -312,9 +373,13 @@ async function handleClear() {
             viewBox="0 0 24 24"
             stroke-width="1.5"
             stroke="currentColor"
-            class="size-6 self-end cursor-pointer"
+            class="size-6 cursor-pointer self-end"
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
           </svg>
 
           <input
@@ -325,7 +390,7 @@ async function handleClear() {
             placeholder="uploadFile?.name"
           />
           <button
-            class="flex flex-row gap-2 items-center h-[50px] hover:text-gray-400"
+            class="flex h-[50px] flex-row items-center gap-2 hover:text-gray-400"
             @click="handleUpload('Document')"
           >
             <svg
@@ -346,7 +411,7 @@ async function handleClear() {
             上传PDF
           </button>
           <button
-            class="flex flex-row gap-2 items-center h-[50px] hover:text-gray-400"
+            class="flex h-[50px] flex-row items-center gap-2 hover:text-gray-400"
             @click="handleUpload('Image')"
           >
             <svg
@@ -367,7 +432,7 @@ async function handleClear() {
             上传图片
           </button>
           <button
-            class="flex flex-row gap-2 items-center h-[50px] hover:text-gray-400"
+            class="flex h-[50px] flex-row items-center gap-2 hover:text-gray-400"
             @click="handleUpload('Audio')"
           >
             <svg
@@ -388,7 +453,7 @@ async function handleClear() {
             上传音频
           </button>
           <button
-            class="flex flex-row gap-2 items-center h-[50px] hover:text-gray-400"
+            class="flex h-[50px] flex-row items-center gap-2 hover:text-gray-400"
             @click="handleUpload('Video')"
           >
             <svg
@@ -411,7 +476,7 @@ async function handleClear() {
         </div>
         <div
           @mouseover="isHover = true"
-          class="bg-gray-900 p-2 rounded-lg text-white hover:opacity-75"
+          class="rounded-lg bg-gray-900 p-2 text-white hover:opacity-75"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -426,20 +491,25 @@ async function handleClear() {
           >
             <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
             <polyline points="17 8 12 3 7 8" />
-            <line x1="12" y1="3" x2="12" y2="15" />
+            <line
+              x1="12"
+              y1="3"
+              x2="12"
+              y2="15"
+            />
           </svg>
         </div>
       </div>
-      <div class="flex-grow relative">
+      <div class="relative flex-grow">
         <input
-          class="w-full border rounded-lg shadow-sm text-lg px-3 h-12 outline-none disabled:text-gray-600"
+          class="h-12 w-full rounded-lg border px-3 text-lg shadow-sm outline-none disabled:text-gray-600"
           type="text"
           placeholder="输入检索内容……"
           v-model="input"
           :disabled="isLoading"
         />
         <button
-          class="w-8 h-8 rounded-lg bg-gray-900 shadow-sm top-2 right-2 absolute text-white flex items-center justify-center hover:opacity-75 disabled:opacity-50"
+          class="absolute right-2 top-2 flex h-8 w-8 items-center justify-center rounded-lg bg-gray-900 text-white shadow-sm hover:opacity-75 disabled:opacity-50"
           @click="handleSend"
           :disabled="isLoading"
         >
@@ -460,7 +530,7 @@ async function handleClear() {
         </button>
       </div>
       <button
-        class="text-gray-600 w-10 h-10 flex items-center justify-center hover:bg-gray-950/5 rounded-full"
+        class="flex h-10 w-10 items-center justify-center rounded-full text-gray-600 hover:bg-gray-950/5"
         @click="handleClear"
       >
         <svg
@@ -477,8 +547,18 @@ async function handleClear() {
           <path d="M3 6h18" />
           <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
           <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-          <line x1="10" x2="10" y1="11" y2="17" />
-          <line x1="14" x2="14" y1="11" y2="17" />
+          <line
+            x1="10"
+            x2="10"
+            y1="11"
+            y2="17"
+          />
+          <line
+            x1="14"
+            x2="14"
+            y1="11"
+            y2="17"
+          />
         </svg>
       </button>
     </div>
