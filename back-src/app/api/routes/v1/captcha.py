9c2ab82639b52ef9e3b585d5172f6abb2f3e27de
AAAkater <1318382761@qq.com>
2024-10-02 23:35:30 +0800

    ✨ feat(backend): add captcha

--- back-src/app/api/routes/v1/captcha.py
+++ back-src/app/api/routes/v1/captcha.py
@@ -1,15 +1,30 @@
 import uuid
 
 from fastapi import APIRouter, HTTPException, status
+from fastapi.responses import RedirectResponse
 
 from app.model import CaptchaItem, GetCaptchaResponse, VerifyGetCaptchaResponse
-from app.utils.captcha_ import (
+from app.utils.captcha_func import (
     generate_captcha_code,
     generate_captcha_img,
     img_to_base64,
 )
+from app.utils.log import logger
+from app.utils.redis_api import redis_client
 
 router = APIRouter()
+
+
+def init_captcha():
+    captcha_id = uuid.uuid4().hex
+    captcha_code = generate_captcha_code()
+    logger.info(f"{captcha_code=}")
+    # * 写入redis 有效期两分钟
+    redis_client.setex(name=captcha_id, time=120, value=captcha_code)
+
+    captcha_img = generate_captcha_img(code=captcha_code)
+    captcha_img_base64: str = img_to_base64(captcha_img)
+    return (captcha_id, captcha_code, captcha_img_base64)
 
 
 @router.get(
@@ -19,14 +34,11 @@ router = APIRouter()
     summary="",
 )
 async def get_captcha() -> GetCaptchaResponse:
-    captcha_id = uuid.uuid4().hex
-    captcha_code = generate_captcha_code()
-    captcha_img = generate_captcha_img(code=captcha_code)
-    captcha_img_base64 = img_to_base64(captcha_img)
+    id, code, img_base64 = init_captcha()
     return GetCaptchaResponse(
         code="0",
         msg="ok",
-        data=CaptchaItem(captchaId=captcha_id, captchaImgBase64=captcha_img_base64),
+        data=CaptchaItem(captchaId=id, captchaImgBase64=img_base64),
     )
 
 
@@ -38,28 +50,33 @@ async def get_captcha() -> GetCaptchaResponse:
 async def verify_captcha(
     captcha_id: str, captcha_code: str
 ) -> VerifyGetCaptchaResponse:
-    # mock
-    if captcha_id == "2fc1176d85aa45589e463a1897397484":
-        if captcha_code == "1234":
-            return VerifyGetCaptchaResponse(code="0", msg="ok", data=None)
-        else:
-            return VerifyGetCaptchaResponse(code="1", msg="验证码错误", data=None)
-    else:
+
+    code = redis_client.get(name=captcha_id)
+    if code is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=f"不存在该验证码:{captcha_id}"
         )
+    logger.info(f"{code=}")
+    if code == captcha_code:
+        return VerifyGetCaptchaResponse(code="0", msg="ok", data=None)
+    else:
+        return VerifyGetCaptchaResponse(code="1", msg="验证码错误", data=None)
 
 
 @router.put(
-    path="/captcha/{captcha_id}",
+    path="/captcha/{old_captcha_id}",
     status_code=status.HTTP_200_OK,
     response_model=GetCaptchaResponse,
 )
-async def refresh_captcha(captcha_id: str) -> GetCaptchaResponse:
-    # mock
-    new_captcha_id = captcha_id
+async def refresh_captcha(old_captcha_id: str):
+
+    # *删除该验证码
+    redis_client.delete(old_captcha_id)
+    logger.info(f"{old_captcha_id=}")
+
+    id, code, img_base64 = init_captcha()
     return GetCaptchaResponse(
         code="0",
         msg="ok",
-        data=CaptchaItem(captchaId=new_captcha_id, captchaImgBase64="asfsafasa"),
+        data=CaptchaItem(captchaId=id, captchaImgBase64=img_base64),
     )
