a306bdda12be23bbaaa4f1e56d7466025eed81dd
AAAkater <1318382761@qq.com>
2024-11-03 22:30:14 +0800

    ✨ feat(backend): verify email captcha apis

--- back-src/app/api/routes/v1/captcha.py
+++ back-src/app/api/routes/v1/captcha.py
@@ -1,15 +1,17 @@
 import uuid
 
-from app.model import CaptchaItem, ResponseBase
+from app.model import CaptchaItem, EmailCaptchaBody, ResponseBase
 from app.utils.captcha_func import (
     generate_captcha_code,
     generate_captcha_img,
     img_to_base64,
 )
+from app.utils.email import send_email
 from app.utils.log import logger
 from app.utils.redis_api import redis_client
 from fastapi import APIRouter, HTTPException, status
-from fastapi.responses import RedirectResponse
+from pydantic import HttpUrl, validate_email
+from pydantic_core import PydanticCustomError
 
 router = APIRouter()
 
@@ -72,3 +74,85 @@ async def refresh_captcha(old_captcha_id: str) -> ResponseBase[CaptchaItem]:
     return ResponseBase[CaptchaItem](
         data=CaptchaItem(captchaId=id, captchaImgBase64=img_base64),
     )
+
+
+@router.get(
+    path="/captcha/email",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBase,
+)
+async def get_email_captcha(
+    item: EmailCaptchaBody,
+) -> ResponseBase:
+    # 校验邮箱格式
+    try:
+        _ = validate_email(value=item.email)
+    except PydanticCustomError as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="邮箱格式错误",
+        )
+    # 查看邮箱验证码是否仍有效
+    if redis_client.exists(item.email):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="验证码请求频繁",
+        )
+
+    captcha_code = generate_captcha_code()
+    # 发送邮箱验证码
+    try:
+        await send_email(
+            email=item.email,
+            captcha=captcha_code,
+        )
+    except Exception as e:
+        logger.error(msg=e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="服务器内部错误",
+        )
+    # 一分钟有效时间
+    redis_client.setex(
+        name=item.email,
+        value=captcha_code,
+        time=60,
+    )
+    return ResponseBase()
+
+
+@router.post(
+    path="/captcha/email",
+    status_code=status.HTTP_200_OK,
+    response_model=ResponseBase,
+)
+async def verify_email_captcha(
+    item: EmailCaptchaBody,
+) -> ResponseBase:
+    # 校验邮箱格式
+    try:
+        _ = validate_email(value=item.email)
+    except PydanticCustomError as e:
+        logger.error(e)
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="邮箱格式错误",
+        )
+
+    # 查看邮箱验证码是否仍有效
+    if not redis_client.exists(item.email):
+        return ResponseBase(
+            code="1",
+            msg="验证码过期",
+        )
+    # 校验验证码
+    code = redis_client.get(name=item.email)
+    if code != item.email_code:
+        redis_client.delete(item.email)
+        return ResponseBase(
+            code="2",
+            msg="验证码错误",
+        )
+
+    return ResponseBase()
--- back-src/app/core/config.py
+++ back-src/app/core/config.py
@@ -1,31 +1,37 @@
-from pydantic import MySQLDsn, computed_field
+from pydantic import PostgresDsn, computed_field
 from pydantic_core import MultiHostUrl
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
 
-    # class Config:
-    #     env_file = ""
-
-    # MYSQL
-    MYSQL_USER: str
-    MYSQL_PASSWORD: str
-    MYSQL_PROT: int
-    MYSQL_SERVER: str
-    MYSQL_DB: str
+    # POSTGRESQL
+    POSTGRESQL_USER: str
+    POSTGRESQL_PASSWORD: str
+    POSTGRESQL_PROT: int = 5432
+    POSTGRESQL_SERVER: str
+    POSTGRESQL_DB: str
 
     @computed_field
     @property
-    def SQLALCHEMY_DATABASE_URI(self) -> MySQLDsn:
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
         return MultiHostUrl.build(
-            scheme="mysql+pymysql",
-            username=self.MYSQL_USER,
-            password=self.MYSQL_PASSWORD,
-            port=self.MYSQL_PROT,
-            host=self.MYSQL_SERVER,
-            path=self.MYSQL_DB,
+            scheme="postgresql+psycopg",
+            username=self.POSTGRESQL_USER,
+            password=self.POSTGRESQL_PASSWORD,
+            port=self.POSTGRESQL_PROT,
+            host=self.POSTGRESQL_SERVER,
+            path=self.POSTGRESQL_DB,
         )
+
+    # EMAIL
+    SMTP_TLS: bool = True
+    SMTP_SSL: bool = False
+    SMTP_PORT: int = 587
+    SMTP_HOST: str | None = None
+    SMTP_USERNAME: str | None = None
+    SMTP_PASSWORD: str | None = None
+    EMAIL_FROM_NAME: str | None = None
 
     # TODO: MILVUS settings
 
